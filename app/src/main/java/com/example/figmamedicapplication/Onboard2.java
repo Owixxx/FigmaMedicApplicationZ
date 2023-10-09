c5d8a3ae86d42b85b501017dfd9da67cf7fe514c
Owixxx <forcollabandother@gmail.com>
2023-10-09 11:46:46 +0300

    Update

--- app/src/main/java/com/example/figmamedicapplication/Onboard2.java
+++ app/src/main/java/com/example/figmamedicapplication/Onboard2.java
@@ -2,7 +2,9 @@ package com.example.figmamedicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Onboard2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Onboard2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboard2);
+    }
+    public void startNewActivity(View v) {
+        Intent intent = new Intent(this, Onboard3.class);
+        startActivity(intent);
     }
 }
